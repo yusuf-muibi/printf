5c28501cf5bdd79cff37a2200dc332588d082e62
yusuf-muibi <muibiyusuf@hotmail.com>
2023-11-11 09:09:56 -0800

    print f

--- _printf.c
+++ _printf.c
@@ -3,17 +3,41 @@
 #include <stddef.h>
 #include <unistd.h>
 /**
+* print_char - Prints a character
+* @args: A list of variadic arguments
+* Return: The number of characters printed
+*/
+static int print_char(va_list args)
+{
+char c = (char)va_arg(args, int);
+return (write(1, &c, 1));
+}
+/**
+* print_string - Prints a string
+* @args: A list of variadic arguments
+* Return: The number of characters printed
+*/
+static int print_string(va_list args)
+{
+char *str;
+int count = 0;
+int t;
+str = va_arg(args, char*);
+if (str == NULL)
+str = "(null)";
+for (t = 0; str[t]; t++)
+count += write(1, &str[t], 1);
+return (count);
+}
+/**
 * _printf - Custom printf function to output formatted data
-* @format: format string containing the specifiers
-* Return: Number of characters printed
+* @format: The format string containing specifiers
+* Return: The number of characters printed
 */
 int _printf(const char *format, ...)
 {
 int count = 0;
 int t;
-int u;
-char temp;
-char *str;
 va_list args;
 va_start(args, format);
 for (t = 0; format && format[t]; t++)
@@ -24,17 +48,10 @@ t++;
 switch (format[t])
 {
 case 'c':
-temp = (char)va_arg(args, int);
-count += write(1, &temp, 1);
+count += print_char(args);
 break;
 case 's':
-{
-str = va_arg(args, char*);
-if (str == NULL)
-str = "(null)";
-for (u = 0; str[u]; u++)
-count += write(1, &str[u], 1);
-}
+count += print_string(args);
 break;
 case '%':
 count += write(1, &format[t], 1);
